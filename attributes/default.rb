8cb36353bc75c754aa5ebd540d7a86d04a636340
Darren Birkett <darren.birkett@gmail.com>
2013-07-17 11:44:30 +0100

    some tailor fixes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,19 +1,19 @@
-default["horizon"]["db"]["username"] = "dash"                                               # node_attribute
-default["horizon"]["db"]["password"] = "dash"                                               # node_attribute
-default["horizon"]["db"]["name"] = "dash"                                                   # node_attribute
+default["horizon"]["db"]["username"] = "dash"
+default["horizon"]["db"]["password"] = "dash"
+default["horizon"]["db"]["name"] = "dash"
 
-default["horizon"]["use_ssl"] = true                                                        # node_attribute
-default["horizon"]["ssl"]["cert"] = "horizon.pem"                                           # node_attribute
-default["horizon"]["ssl"]["key"] = "horizon.key"                                            # node_attribute
+default["horizon"]["use_ssl"] = true
+default["horizon"]["ssl"]["cert"] = "horizon.pem"
+default["horizon"]["ssl"]["key"] = "horizon.key"
 
-default["horizon"]["services"]["dash"]["scheme"] = "http"                       # node_attribute
-default["horizon"]["services"]["dash"]["network"] = "public"                    # node_attribute
-default["horizon"]["services"]["dash"]["port"] = 80                           # node_attribute
+default["horizon"]["services"]["dash"]["scheme"] = "http"
+default["horizon"]["services"]["dash"]["network"] = "public"
+default["horizon"]["services"]["dash"]["port"] = 80
 default["horizon"]["services"]["dash"]["path"] = "/"
 
-default["horizon"]["services"]["dash_ssl"]["scheme"] = "https"                       # node_attribute
-default["horizon"]["services"]["dash_ssl"]["network"] = "public"                    # node_attribute
-default["horizon"]["services"]["dash_ssl"]["port"] = 443                           # node_attribute
+default["horizon"]["services"]["dash_ssl"]["scheme"] = "https"
+default["horizon"]["services"]["dash_ssl"]["network"] = "public"
+default["horizon"]["services"]["dash_ssl"]["port"] = 443
 default["horizon"]["services"]["dash_ssl"]["path"] = "/"
 
 default["horizon"]["swift"]["enabled"] = "False"
@@ -22,31 +22,32 @@ default["horizon"]["theme"] = "default"
 
 case node["platform"]
 when "fedora", "centos", "redhat", "amazon", "scientific"
-  default["horizon"]["ssl"]["dir"] = "/etc/pki/tls"                                         # node_attribute
-  default["horizon"]["local_settings_path"] = "/etc/openstack-dashboard/local_settings"     # node_attribute
+  default["horizon"]["ssl"]["dir"] = "/etc/pki/tls"
+  default["horizon"]["local_settings_path"] = "/etc/openstack-dashboard/local_settings"
   # TODO(shep) - Fedora does not generate self signed certs by default
-  default["horizon"]["platform"] = {                                                   # node_attribute
-    "supporting_packages" => [ "MySQL-python",  "python-cinderclient",
-                           "python-quantumclient", "python-keystoneclient", "python-glanceclient",
-                           "python-novaclient"],
+  default["horizon"]["platform"] = {
+    "supporting_packages" => ["MySQL-python", "python-cinderclient",
+      "python-quantumclient", "python-keystoneclient", "python-glanceclient",
+      "python-novaclient"],
     "horizon_packages" => ["openstack-dashboard", "python-netaddr"],
     "package_overrides" => ""
   }
-  default["horizon"]["dash_path"] = "/usr/share/openstack-dashboard"      # node_attribute
-  default["horizon"]["stylesheet_path"] = "/usr/share/openstack-dashboard/openstack_dashboard/templates/_stylesheets.html"
-  default["horizon"]["wsgi_path"] = node["horizon"]["dash_path"] + "/openstack_dashboard/wsgi"                    # node_attribute
+  default["horizon"]["dash_path"] = "/usr/share/openstack-dashboard"
+  default["horizon"]["stylesheet_path"] =
+    "/usr/share/openstack-dashboard/openstack_dashboard/templates/_stylesheets.html"
+  default["horizon"]["wsgi_path"] = node["horizon"]["dash_path"] + "/openstack_dashboard/wsgi"
 when "ubuntu", "debian"
-  default["horizon"]["ssl"]["dir"] = "/etc/ssl"                                             # node_attribute
-  default["horizon"]["local_settings_path"] = "/etc/openstack-dashboard/local_settings.py"  # node_attribute
-  default["horizon"]["platform"] = {                                                   # node_attribute
+  default["horizon"]["ssl"]["dir"] = "/etc/ssl"
+  default["horizon"]["local_settings_path"] = "/etc/openstack-dashboard/local_settings.py"
+  default["horizon"]["platform"] = {
     "supporting_packages" => ["python-mysqldb", "python-cinderclient",
-                           "python-quantumclient", "python-keystoneclient", "python-glanceclient",
-                           "python-novaclient"],
-    "horizon_packages" => ["lessc","openstack-dashboard", "python-netaddr"], 
+      "python-quantumclient", "python-keystoneclient", "python-glanceclient",
+      "python-novaclient"],
+    "horizon_packages" => ["lessc", "openstack-dashboard", "python-netaddr"],
     "package_overrides" => "-o Dpkg::Options::='--force-confold' -o Dpkg::Options::='--force-confdef'"
   }
-  default["horizon"]["dash_path"] = "/usr/share/openstack-dashboard/openstack_dashboard"      # node_attribute
-  default["horizon"]["stylesheet_path"] = "/usr/share/openstack-dashboard/openstack_dashboard/templates/_stylesheets.html"
-  default["horizon"]["wsgi_path"] = node["horizon"]["dash_path"] + "/wsgi"                    # node_attribute
+  default["horizon"]["dash_path"] = "/usr/share/openstack-dashboard/openstack_dashboard"
+  default["horizon"]["stylesheet_path"] =
+    "/usr/share/openstack-dashboard/openstack_dashboard/templates/_stylesheets.html"
+  default["horizon"]["wsgi_path"] = node["horizon"]["dash_path"] + "/wsgi"
 end
-
