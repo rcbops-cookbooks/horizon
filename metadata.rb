3a15956738d34efc90490108461086cd0a1dbda0
rackerjoe <breu@breu.org>
2013-09-09 16:21:09 -0500

    Updating version # to 4.1.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Rackspace US, Inc."
 license          "Apache 2.0"
 description      "Installs/Configures horizon"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.16"
+version          "4.1.2"
 
 %w{ centos ubuntu }.each do |os|
   supports os
