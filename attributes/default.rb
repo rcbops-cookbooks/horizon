c23d06a8ae437a2d504c27d2314d2f1866c5cf98
cloudnull <kevin.carter@rackspace.com>
2013-10-24 09:55:11 -0500

    removed unneeded attrs

--- attributes/default.rb
+++ attributes/default.rb
@@ -21,49 +21,50 @@
 default["horizon"]["horizon_user"] = "horizon"
 default["horizon"]["rax_logo"] = true
 
+# Setup Database creds
 default["horizon"]["db"]["username"] = "dash"
 default["horizon"]["db"]["password"] = "dash"
 default["horizon"]["db"]["name"] = "dash"
 
+# General SSL Setup
 default["horizon"]["use_ssl"] = true
 default["horizon"]["ssl"]["cert"] = "horizon.pem"
 default["horizon"]["ssl"]["key"] = "horizon.key"
 
+# Horizon Services
 default["horizon"]["services"]["dash"]["scheme"] = "http"
 default["horizon"]["services"]["dash"]["network"] = "public"
 default["horizon"]["services"]["dash"]["port"] = 80
 default["horizon"]["services"]["dash"]["path"] = "/"
 
+# Horizon SSL Services
 default["horizon"]["services"]["dash_ssl"]["scheme"] = "https"
 default["horizon"]["services"]["dash_ssl"]["network"] = "public"
 default["horizon"]["services"]["dash_ssl"]["port"] = 443
 default["horizon"]["services"]["dash_ssl"]["path"] = "/"
 
+# Enable / Disable Swift panel
 default["horizon"]["swift"]["enabled"] = "False"
 
+# Set Default Theme, Supports "default" or "ubuntu"
 default["horizon"]["theme"] = "default"
-default["horizon"]["theme_image_update_style"] = "download_list"
-default["horizon"]["theme_image_base"] = "http://ef550cb0f0ed69a100c1-40806b80b9b0290f6d33c73b927ee053.r51.cf2.rackcdn.com"
-default["horizon"]["theme_image_list"] = ["PrivateCloud.png", "Rackspace_Cloud_Company.png",
-    "Rackspace_Cloud_Company_Small.png", "alert_red.png", "body_bkg.gif",
-    "selected_arrow.png"]
-default["horizon"]["theme_css_update_style"] = "cookbook_list"
-default["horizon"]["theme_css_base"] = nil
-case node["horizon"]["theme"]
-when "ubuntu"
-  default["horizon"]["theme_style_template"] = "ubuntu_stylesheets.html.erb"
-  default["horizon"]["theme_css_list"] = nil
-when "Rackspace"
-  default["horizon"]["theme_style_template"] = "rs_stylesheets.html.erb"
-  default["horizon"]["theme_css_list"] = ["folsom.css"]
-else
-  default["horizon"]["theme_style_template"] = "default_stylesheets.html.erb"
-  default["horizon"]["theme_css_list"] = nil
-end
 
-default["horizon"]["help_url"] = "http://docs.openstack.org"
-default["horizon"]["password_autocomplete"] = "off"
-default["horizon"]["theme_image_base"] = "http://ef550cb0f0ed69a100c1-40806b80b9b0290f6d33c73b927ee053.r51.cf2.rackcdn.com"
+# Options Customize the theme images
+default["horizon"]["theme_image_update_style"] = false
+default["horizon"]["theme_image_base"] = nil
+default["horizon"]["theme_image_list"] = nil
+
+# Options Customize the theme css
+default["horizon"]["theme_css_update_style"] = false
+default["horizon"]["theme_css_base"] = nil
+default["horizon"]["theme_css_list"] = nil
+
+# Set the Help URL in Horizon
+default["horizon"]["help_url"] = "http://www.rackspace.com/knowledge_center/product-page/rackspace-private-cloud"
+
+# Enable Password Auto Complete
+default["horizon"]["password_autocomplete"] = "on"
+
 # domains that horizon accepts traffic to (http://myopenstackdashboard.com, for example)
 default["horizon"]["allowed_hosts"] = nil # set to an array of strings if you want to lock horizon down.
 
@@ -73,32 +74,42 @@ default["horizon"]["endpoint_host"] = nil
 default["horizon"]["endpoint_port"] = nil
 default["horizon"]["endpoint_scheme"] = nil
 
+# Set platform options
+default["horizon"]["local_settings_path"] = "/etc/openstack-dashboard/local_settings"
+default["horizon"]["secret_key"] = "/etc/openstack-dashboard/secret_key"
+default["horizon"]["stylesheet_path"] = "/usr/share/openstack-dashboard/openstack_dashboard/templates/_stylesheets.html"
+
 case node["platform"]
-when "fedora", "centos", "redhat", "amazon", "scientific"
-  default["horizon"]["ssl"]["dir"] = "/etc/pki/tls"
-  default["horizon"]["secret_key"] = "/etc/openstack-dashboard/secret_key"
-  default["horizon"]["local_settings_path"] = "/etc/openstack-dashboard/local_settings"
-  # TODO(shep) - Fedora does not generate self signed certs by default
-  default["horizon"]["platform"] = {
-    "supporting_packages" => ["MySQL-python", "python-cinderclient", "python-neutronclient", "python-keystoneclient", "python-glanceclient", "python-novaclient"],
-    "horizon_packages" => ["openstack-dashboard", "python-netaddr", "nodejs-less"],
-    "package_options" => ""
-  }
-  default["horizon"]["dash_path"] = "/usr/share/openstack-dashboard"
-  default["horizon"]["stylesheet_path"] = "/usr/share/openstack-dashboard/openstack_dashboard/templates/_stylesheets.html"
-  default["horizon"]["wsgi_path"] = node["horizon"]["dash_path"] + "/openstack_dashboard/wsgi"
-when "ubuntu", "debian"
-  default["horizon"]["ssl"]["dir"] = "/etc/ssl"
-  default["horizon"]["secret_key"] = "/etc/openstack-dashboard/secret_key"
-  default["horizon"]["local_settings_path"] = "/etc/openstack-dashboard/local_settings.py"
-  default["horizon"]["platform"] = {
-    "supporting_packages" => ["python-mysqldb", "python-cinderclient",
-      "python-neutronclient", "python-keystoneclient", "python-glanceclient",
-      "python-novaclient"],
-    "horizon_packages" => ["openstack-dashboard", "python-netaddr", "node-less"],
-    "package_options" => "-o Dpkg::Options::='--force-confold' -o Dpkg::Options::='--force-confdef'"
-  }
-  default["horizon"]["dash_path"] = "/usr/share/openstack-dashboard/openstack_dashboard"
-  default["horizon"]["stylesheet_path"] = "/usr/share/openstack-dashboard/openstack_dashboard/templates/_stylesheets.html"
-  default["horizon"]["wsgi_path"] = node["horizon"]["dash_path"] + "/wsgi"
+  when "fedora", "centos", "redhat", "amazon", "scientific"
+    default["horizon"]["dash_path"] = "/usr/share/openstack-dashboard"
+    default["horizon"]["wsgi_path"] = node["horizon"]["dash_path"] + "/openstack_dashboard/wsgi"
+    default["horizon"]["ssl"]["dir"] = "/etc/pki/tls"
+    default["horizon"]["platform"] = {
+      "supporting_packages" => ["MySQL-python",
+                                "python-cinderclient",
+                                "python-neutronclient",
+                                "python-keystoneclient",
+                                "python-glanceclient",
+                                "python-novaclient"],
+      "horizon_packages" => ["openstack-dashboard",
+                             "python-netaddr",
+                             "nodejs-less"],
+      "package_options" => ""
+    }
+  when "ubuntu", "debian"
+    default["horizon"]["dash_path"] = "/usr/share/openstack-dashboard/openstack_dashboard"
+    default["horizon"]["wsgi_path"] = node["horizon"]["dash_path"] + "/wsgi"
+    default["horizon"]["ssl"]["dir"] = "/etc/ssl"
+    default["horizon"]["platform"] = {
+      "supporting_packages" => ["python-mysqldb",
+                                "python-cinderclient",
+                                "python-neutronclient",
+                                "python-keystoneclient",
+                                "python-glanceclient",
+                                "python-novaclient"],
+      "horizon_packages" => ["openstack-dashboard",
+                             "python-netaddr",
+                             "node-less"],
+      "package_options" => "-o Dpkg::Options::='--force-confold' -o Dpkg::Options::='--force-confdef'"
+    }
 end
